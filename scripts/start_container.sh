b60cbcbc07b575945ac46179e84afa303ed03597
SuprithGogireddy <SGogireddy288@everestdx.com>
2026-03-11 20:30:04 +0530

    picking latest docker image

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 set -e
-#pull the docker image from docker hub
-docker pull suprithreddy789/deployment-practice::latest
 
-#run the docker image as a container
-docker run -d -p 8080:8080 suprithreddy789/deployment-practice::latest
+# pull the docker image from docker hub
+docker pull suprithreddy789/deployment-practice:latest
+
+# stop and remove any old container
+docker stop deployment-practice || true
+docker rm deployment-practice || true
+
+# run the docker image as a container
+docker run -d -p 8080:8080 --name deployment-practice suprithreddy789/deployment-practice:latest
