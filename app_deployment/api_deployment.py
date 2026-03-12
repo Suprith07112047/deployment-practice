d58b310d2bf99be0cb123d43756dcbf11af358f0
SuprithGogireddy <SGogireddy288@everestdx.com>
2026-03-12 15:18:04 +0530

    change

--- app_deployment/api_deployment.py
+++ app_deployment/api_deployment.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
 api_code_deployment = APIRouter()
-@api_code_deployment.post("/azure-cloud/kubernetes/api-code-deployment")
+@api_code_deployment.post("/aws-cloud/ec2/api-code-deployment")
 def deployed_function1():
-    return {"message": "Hello From Azure Kubernetes"}
+    return {"message": "Hello From AWS Cloud EC2 API Code Deployment!"}
