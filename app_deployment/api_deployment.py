d7dc109f312a7dcd585ecf4cf0f8c5ce308d773b
Suprith07112047 <Suprith.Gogireddy@everestdx.com>
2026-06-23 11:12:10 +0530

    Update api_deployment.py

--- app_deployment/api_deployment.py
+++ app_deployment/api_deployment.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 api_code_deployment = APIRouter()
 @api_code_deployment.post("/aws-cloud/ec2/api-code-deployment")
 def deployed_function1():
-    return {"message": "Hello From AWS Cloud EC2 API Code Deployment!"}
+    return {"message": "Hello From AWS Cloud EC2 API Code Deployment By CI CD!"}
 
 
 # Basic GET endpoint
